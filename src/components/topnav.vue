84640c35d91f610fa4cfad7b3b271273ea2edf90
Jens <jenshabermann89@yahoo.com>
2020-08-14 15:28:07 +0200

    Topnav Buttons werden automatisch erstellt

--- src/components/topnav.vue
+++ src/components/topnav.vue
@@ -1,36 +1,34 @@
 <template>
   <div>
-
-    top Nav Componente
-    <button @click="getTitel"></button>
-    
+    <button v-for="Thema in jsondata" v-bind:key="Thema">{{Thema.Titel}}</button>
   </div>
 </template>
 
 <script>
-import json from '../json/nav.json'
+import json from "../json/nav.json";
 export default {
-  name: 'topnav',
-  props: {
+  name: "topnav",
+  props: {},
+  data: function () {
+    return {
+      jsondata: null,
+    };
+  },
+
+  methods: {
     
   },
-  methods:{
-
-    getTitel(){
-
-     console.log(json.nav)
-
-    },
-
-  }
-
-}
+  created() {
+    this.jsondata = json.nav;
+    console.log(this.jsondata);
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-div{width: 100%;
-height: 100%;
+div {
+  width: 100%;
+  height: 100%;
 }
 </style>
